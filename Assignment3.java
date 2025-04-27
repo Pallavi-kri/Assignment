488584533772c6d18cab4badfb63e0d01b6d6021
pallavi <pallavikumari18102003@gimal.com>
2025-04-27 16:02:37 +0530

    add file

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+// Java Program to check whether a given character is alphabet or not
+import java.util.*;
+public class Assignment3 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a character:");
+      char ch = sc.next().charAt(0);
+    if(ch >= 'A' && ch<='Z' || ch>='a' && ch<='z')
+    {
+        System.out.println(ch+" is an alphabet");
+    }
+    else{
+        System.out.println(ch+" is not an alphabet");
+    }
+ }
+}
